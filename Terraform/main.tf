be0366aea1f033ee8d94252d032465b293912af6
Dhananjay Khirdikar <khirdikardhananjay2310@gmail.com>
2025-12-07 14:04:36 +0530

    latest

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,25 +1,23 @@
 resource "aws_instance" "studentapp" {
-    ami = var.studentapp_ami
-    instance_type = var.studentapp_instance_type
-    key_name = var.studentapp_key_name
-    vpc_security_group_ids = [ var.studentapp_instance_vpc_security_group_ids ]
-    disable_api_termination = var.studentapp_disable_api_termination
+  ami                         = var.studentapp_ami
+  instance_type               = var.studentapp_instance_type
+  key_name                    = var.studentapp_key_name
+  vpc_security_group_ids      = [var.studentapp_instance_vpc_security_group_ids]
+  disable_api_termination     = var.studentapp_disable_api_termination
 
-    user_data = <<-EOT
-      #!/bin/bash
-      sudo apt update -y
-
-      git clone https://github.com/er-dhananjay/Studentapp_Project.git
-      
-      chmod 700 dockerinstall.sh
-
-      sh dockerinstall.sh
-
-      docker compose up -d
-      
+  user_data = <<-EOT
+    #!/bin/bash
+    sudo apt update -y
+    
+    git clone https://github.com/er-dhananjay/Studentapp_Project.git
+    
+    chmod 700 dockerinstall.sh
+    sh dockerinstall.sh
+    
+    docker compose up -d
+  EOT
 }
 
 output "studentapp_publicip" {
   value = aws_instance.studentapp.public_ip
 }
-
