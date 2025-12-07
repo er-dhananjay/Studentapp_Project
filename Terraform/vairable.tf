66f1671f29720438bd3cd70cada487b63368e183
Dhananjay Khirdikar <khirdikardhananjay2310@gmail.com>
2025-12-07 14:21:19 +0530

    done

--- Terraform/vairable.tf
+++ Terraform/vairable.tf
@@ -2,13 +2,13 @@ variable "studentapp_instance_type" {
     default = "t3.micro"
 }
 variable "studentapp_ami" {
-    default = "ami-0f00d706c4a80fd93"
+    default = "ami-0d176f79571d18a8f"
 }
 variable "studentapp_key_name" {
-    default = "mykey"
+    default = "dk"
 }
 variable "studentapp_instance_vpc_security_group_ids" {
-    default = "sg-0b4ace81d68dd0b6d"
+    default = "sg-0f2758b7b37e072cf"
 }
 variable "studentapp_disable_api_termination" {
     default = false
